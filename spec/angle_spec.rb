ada20cbebc69481ff02ee0f58e9db75cdb698b69
Maksim <max.edlenko@gmail.com>
2026-03-20 19:49:21 +0300

    test: wrote tests for predicates

--- spec/angle_spec.rb
+++ spec/angle_spec.rb
@@ -44,7 +44,68 @@ describe Angle do
       expect(angle.degrees).to be_within(1e-10).of(90)
     end
   end
+   # 3. ПРЕДИКАТЫ
+  describe '#acute?' do
+    it 'возвращает true для острого угла (< 90°)' do
+      expect(Angle.new(Math::PI / 4)).to be_acute
+    end
 
+    it 'возвращает false для прямого угла' do
+      expect(Angle.new(Math::PI / 2)).not_to be_acute
+    end
+
+    it 'возвращает false для тупого угла' do
+      expect(Angle.new(3 * Math::PI / 4)).not_to be_acute
+    end
+  end
+
+  describe '#right?' do
+    it 'возвращает true для прямого угла (90°)' do
+      expect(Angle.new(Math::PI / 2)).to be_right
+    end
+
+    it 'возвращает false для острого угла' do
+      expect(Angle.new(Math::PI / 4)).not_to be_right
+    end
+
+    it 'возвращает false для тупого угла' do
+      expect(Angle.new(3 * Math::PI / 4)).not_to be_right
+    end
+  end
+
+  describe '#obtuse?' do
+    it 'возвращает true для тупого угла (90° < angle < 180°)' do
+      expect(Angle.new(3 * Math::PI / 4)).to be_obtuse
+    end
+
+    it 'возвращает false для острого угла' do
+      expect(Angle.new(Math::PI / 4)).not_to be_obtuse
+    end
+
+    it 'возвращает false для прямого угла' do
+      expect(Angle.new(Math::PI / 2)).not_to be_obtuse
+    end
+  end
+
+  describe '#straight?' do
+    it 'возвращает true для развёрнутого угла (180°)' do
+      expect(Angle.new(Math::PI)).to be_straight
+    end
+
+    it 'возвращает false для других углов' do
+      expect(Angle.new(Math::PI / 2)).not_to be_straight
+    end
+  end
+
+  describe '#reflex?' do
+    it 'возвращает true для рефлекторного угла (> 180°)' do
+      expect(Angle.new(5 * Math::PI / 4)).to be_reflex
+    end
+
+    it 'возвращает false для угла <= 180°' do
+      expect(Angle.new(Math::PI)).not_to be_reflex
+    end
+  end
     
     
 
